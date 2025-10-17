ad417be3b9fd12fde2700ee0916d3178453d2b01
Shakirat Akanji <akanjishakirato@gmail.com>
2025-10-17 22:17:33 +0100

    Update task 1

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -16,8 +15,8 @@ def create_database():
             mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
             print("Database 'alx_book_store' created successfully!")
-    except Error as e:
-        print(f"Error Connecting to MySQL: {e}")
+    except mysql.connector.Error as err:
+        print(f"Error Connecting to MySQL: {err}")
     finally:
         # Close the connection
         if mydb.is_connected():
